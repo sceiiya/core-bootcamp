b9744f449802ce272609da5771c9ab439c473b36
sceiiya <meetscheidj@gmail.com>
2025-02-08 16:25:59 +0800

    chore: test 01-solidity

--- 01-solidity/contracts/01_solidity_data_types.sol
+++ 01-solidity/contracts/01_solidity_data_types.sol
@@ -5,6 +5,7 @@ import "hardhat/console.sol";
 contract SolidityDataTypes {
     uint256 balance;
     address owner;
+
     mapping(string => uint256) accounts;
 
     constructor() {
@@ -12,11 +13,13 @@ contract SolidityDataTypes {
         owner = address(0);
         accounts["theblokc"] = 999999;
 
+
+
         // TODO: print the values above using console.log
-        // console.log("balance is ", balance);
+        console.log("balance is ", balance);
 
-        // console.log("owner is ", owner);
+        console.log("owner is ", owner);
 
-        // console.log("account is ", accounts["theblokc"]);
+        console.log("account is ", accounts["theblokc"]);
     }
 }
--- 01-solidity/contracts/02_solidity_constructor.sol
+++ 01-solidity/contracts/02_solidity_constructor.sol
@@ -6,8 +6,8 @@ contract SolidityConstructor {
     uint256 balance = 0;
 
     constructor(uint256 initialBalance) {
-        // balance = initialBalance;
+        balance = initialBalance;
 
-        // console.log("balance is now ", balance);
+        console.log("balance is now ", balance);
     }
 }
